f51cd9d22584ad19a2436e47f1d877b1ca80eedd
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-13 09:25:09 -0300

    chore: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+import 'dotenv/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap().then(() =>
-  Logger.log('Application is listening on port 3000', 'Bootstrap'),
+  Logger.log(
+    `Application is listening on port ${process.env.PORT || 3000}`,
+    'Bootstrap',
+  ),
 );
