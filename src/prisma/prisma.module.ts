0e5a54bf77d76c6654cf9dfa41dd00444c5bce92
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-13 12:05:33 -0300

    feat: create filter deleted hook

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,8 +1,62 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '@prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
 })
-export class PrismaModule {}
+export class PrismaModule {
+  /**
+   * ------------------------------------------------------
+   * Hooks
+   * ------------------------------------------------------
+   */
+  static filterDeletedRecords(params: Prisma.MiddlewareParams, next) {
+    if (Object.values(PrismaActions).includes(params.action)) {
+      params.args.where = {
+        is_deleted: false,
+        ...params.args.where,
+      };
+    }
+
+    return next(params);
+  }
+}
+
+export type PrismaAction =
+  | 'findUnique'
+  | 'findMany'
+  | 'findFirst'
+  | 'create'
+  | 'createMany'
+  | 'update'
+  | 'updateMany'
+  | 'upsert'
+  | 'delete'
+  | 'deleteMany'
+  | 'executeRaw'
+  | 'queryRaw'
+  | 'aggregate'
+  | 'count'
+  | 'runCommandRaw'
+  | 'findRaw';
+
+export const PrismaActions: PrismaAction[] = [
+  'findUnique',
+  'findMany',
+  'findFirst',
+  'create',
+  'createMany',
+  'update',
+  'updateMany',
+  'upsert',
+  'delete',
+  'deleteMany',
+  'executeRaw',
+  'queryRaw',
+  'aggregate',
+  'count',
+  'runCommandRaw',
+  'findRaw',
+];
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,14 @@
 import { INestApplication as App, Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { UserEntity } from '@user/entities/user.entity';
+import { PrismaModule } from '@prisma/prisma.module';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   async onModuleInit() {
     await this.$connect();
 
+    this.$use(PrismaModule.filterDeletedRecords);
     this.$use(UserEntity.hashPassword);
   }
 
