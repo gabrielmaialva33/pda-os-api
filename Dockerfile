c8940341d35d09196e11f47ba3093380656c9318
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-24 21:37:11 -0300

    fix: dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ WORKDIR /home/app
 
 # Install app dependencies
 COPY package*.json ./
-COPY prisma ./prisma/
+COPY tsconfig.json ./
+COPY tsconfig.build.json ./
 
 RUN yarn
 
@@ -19,14 +20,9 @@ FROM node:18 AS runner
 WORKDIR /home/app
 
 # Copy the build files
-COPY --from=builder /home/app/node_modules ./node_modules
-COPY --from=builder /home/app/package*.json ./
-COPY --from=builder /home/app/dist ./dist
-COPY --from=builder /home/app/prisma ./prisma
+COPY --from=builder /home/app .
 
 EXPOSE 3333
 
 # Run the app
 CMD [ "yarn", "start:migrate:prod" ]
-
-# CMD [ "node", "dist/main.js" ]
--- src/database/migrations/migration.20221124152330.ts
+++ src/database/migrations/migration.20221124152330.ts
@@ -2,6 +2,8 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20221124152330 extends Migration {
   async up(): Promise<void> {
+    this.addSql('create extension if not exists "uuid-ossp";');
+
     this.addSql(
       'create table "users" ("id" uuid not null default uuid_generate_v4(), "created_at" timestamptz(0) not null default now(), "updated_at" timestamptz(0) not null default now(), "deleted_at" timestamptz(0) null, "first_name" varchar(80) not null, "last_name" varchar(80) not null, "full_name" varchar(160) generated always as (first_name || \' \' || last_name) stored null, "email" varchar(255) not null, "user_name" varchar(50) not null, "password" varchar(118) not null, "is_online" boolean not null default false, constraint "users_pkey" primary key ("id"));',
     );
