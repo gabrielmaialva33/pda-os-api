2a42bbc28d9d2e95c0bee159291da9a5da95f8f0
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-27 16:19:07 -0300

    refactor: main global nestjs

--- src/main.ts
+++ src/main.ts
@@ -17,25 +17,48 @@ import { LoggerModule } from '@logger/logger.module';
 import { LoggerService } from '@logger/services/logger.service';
 import { UserModule } from '@user/user.module';
 import { UserService } from '@user/services/user.service';
+import helmet from '@fastify/helmet';
+import { FastifyReply, FastifyRequest } from 'fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { bufferLogs: true },
   );
 
+  /**
+   * ------------------------------------------------------
+   * Security
+   * ------------------------------------------------------
+   */
+  await app.register(helmet);
+  app.enableCors();
+
+  /**
+   * ------------------------------------------------------
+   * Middleware
+   * ------------------------------------------------------
+   */
   const fastify = app.getHttpAdapter().getInstance();
-  fastify.addHook('onRequest', async (request, reply) => {
-    await new LoggerMiddleware(
-      app.select(LoggerModule).get(LoggerService),
-      app.select(UserModule).get(UserService),
-    ).onRequest(request, reply);
-  });
+  fastify.addHook(
+    'onRequest',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      await new LoggerMiddleware(
+        app.select(LoggerModule).get(LoggerService),
+        app.select(UserModule).get(UserService),
+      ).onRequest(request, reply);
+    },
+  );
+
+  /**
+   * ------------------------------------------------------
+   * Global Config
+   * ------------------------------------------------------
+   */
+  app.useGlobalPipes(new ValidationPipe(ValidationPipeConfig));
 
   useContainer(app.select(CommonModule), { fallbackOnErrors: true });
-
-  app.useGlobalPipes(new ValidationPipe(ValidationPipeConfig));
-  app.enableCors();
 
   await app.listen(process.env.PORT || 3333, process.env.HOST || '0.0.0.0');
 }
