6b528c5fecaf407dbc362b633d0e5cf29d498555
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-28 20:40:26 -0300

    refactor: auth exceptions

--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -1,11 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '@auth/services/auth.service';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly i18n: I18nService,
+  ) {
     super({
       usernameField: 'uid',
       passwordField: 'password',
@@ -14,7 +18,12 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(uid: string, password: string): Promise<any> {
     const user = await this.authService.validate(uid, password);
-    if (!user) throw new UnauthorizedException();
+    if (!user)
+      throw new UnauthorizedException({
+        message: this.i18n.t(`exception.Unauthorized`),
+        status: 401,
+        display: true,
+      });
     return user;
   }
 }
